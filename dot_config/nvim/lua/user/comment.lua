f8fd722e84d00468a1af883754943d04dad42fe1
Gethin Davies <grd.davies@gmail.com>
2022-07-14 00:12:50 +0100

    match vscode comment keymap

--- dot_config/nvim/lua/user/comment.lua
+++ dot_config/nvim/lua/user/comment.lua
@@ -4,6 +4,52 @@ if not status_ok then
 end
 
 comment.setup {
+
+    ---LHS of toggle mappings in NORMAL + VISUAL mode
+    ---@type table
+    toggler = {
+        ---Line-comment toggle keymap
+        line = '<M-/>', -- default = 'gcc'
+        ---Block-comment toggle keymap
+        block = 'gcb', -- default = 'gbc'
+    },
+
+    ---LHS of operator-pending mappings in NORMAL + VISUAL mode
+    ---@type table
+    opleader = {
+        ---Line-comment keymap
+        line = 'gc',
+        ---Block-comment keymap
+        block = 'gb',
+    },
+
+    ---LHS of extra mappings
+    ---@type table
+    extra = {
+        ---Add comment on the line above
+        above = 'gcO',
+        ---Add comment on the line below
+        below = 'gco',
+        ---Add comment at the end of line
+        eol = 'gcA',
+    },
+
+    ---Create basic (operator-pending) and extended mappings for NORMAL + VISUAL mode
+    ---NOTE: If `mappings = false` then the plugin won't create any mappings
+    ---@type boolean|table
+    mappings = {
+        ---Operator-pending mapping
+        ---Includes `gcc`, `gbc`, `gc[count]{motion}` and `gb[count]{motion}`
+        ---NOTE: These mappings can be changed individually by `opleader` and `toggler` config
+        basic = true,
+        ---Extra mapping
+        ---Includes `gco`, `gcO`, `gcA`
+        extra = true,
+        ---Extended mapping
+        ---Includes `g>`, `g<`, `g>[count]{motion}` and `g<[count]{motion}`
+        extended = false,
+    },
+
   ---@param ctx CommentCtx
   pre_hook = function(ctx)
     local U = require "Comment.utils"
