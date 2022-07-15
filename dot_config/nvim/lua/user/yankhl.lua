995949bed43d6a33128985e4688b9367a4c43e83
Gethin Davies <grd.davies@gmail.com>
2022-07-15 01:46:36 +0100

    :us: Add yank hl augroup
    Fixes yank highlighting when running under vscode

--- dot_config/nvim/lua/user/yankhl.lua
+++ dot_config/nvim/lua/user/yankhl.lua
@@ -5,7 +5,10 @@ local opts = {
   timeout = 150,
 }
 
+local yankGrp = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function() vim.highlight.on_yank(opts) end,
+  group = yankGrp,
 })
 
