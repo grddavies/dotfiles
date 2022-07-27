aa972a5ba4c75ffe3667786070885f9fad3ede4c
Gethin Davies <grd.davies@gmail.com>
2022-07-27 09:59:01 +0100

    Split options for vscode nvim
    Disabling features such as whitespace rendering and line numbers when
    running under vscode improves performance

--- home/dot_config/nvim/lua/user/options.lua
+++ home/dot_config/nvim/lua/user/options.lua
@@ -1,44 +1,58 @@
-local options = {
+-- Options relevant to neovim in Standalone and running under VScode-nvim
+local shared_options = {
   backup = false,                          -- create a backup file
-  cmdheight = 1,                           -- Nvim command line height
-  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
   conceallevel = 0,                        -- so that `` is visible in markdown files
   fileencoding = "utf-8",                  -- the encoding written to a file
   hlsearch = true,                         -- highlight all matches on previous search pattern
   ignorecase = true,                       -- ignore case in search patterns
   smartcase = true,                        -- smart case: Use case sensitive search if uppercase chars in query
-  mouse = "a",                             -- allow the mouse to be used in neovim
   pumheight = 10,                          -- pop up menu height
-  showmode = false,                        -- we don't need to see things like -- INSERT -- anymore
+  showmode = true,                        -- Toggle mode display -- INSERT -- anymore
   showtabline = 2,                         -- always show tabs
-  smartindent = true,                      -- make indenting smarter again
   splitbelow = true,                       -- force all horizontal splits to go below current window
   splitright = true,                       -- force all vertical splits to go to the right of current window
   swapfile = false,                        -- creates a swapfile
   termguicolors = true,                    -- set term gui colors (most terminals support this)
   timeoutlen = 100,                        -- time to wait for a mapped sequence to complete (in milliseconds)
   undofile = true,                         -- enable persistent undo
-  updatetime = 1000,                       -- time until completions offered (4000ms default)
   writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
   expandtab = true,                        -- convert tabs to spaces
   shiftwidth = 2,                          -- the number of spaces inserted for each indentation
   tabstop = 2,                             -- insert 2 spaces for a tab
+  scrolloff = 8,                           -- is one of my fav
+  sidescrolloff = 8,
+  startofline = true,                      -- retain cursor position where possible
+}
+
+-- Options that don't apply to running VScode-nvim
+local standalone_options = {
+  smartindent = true,                      -- make indenting smarter again
+  cmdheight = 1,                           -- Nvim command line height
+  completeopt = { "menuone", "noselect" }, -- completion params?
+  updatetime = 1000,                       -- time until completions offered (4000ms default)
+  mouse = "a",                             -- allow the mouse to be used in neovim
   cursorline = true,                       -- highlight the current line
-  cursorlineopt = 'number',                 -- only highlight current line's number
+  cursorlineopt = 'number',                -- only highlight current line's number
   number = true,                           -- set numbered lines
   relativenumber = true,                   -- set relative numbered lines
   numberwidth = 4,                         -- set number column width to 2 {default 4}
   signcolumn = "yes",                      -- set the number column as the sign column
   wrap = false,                            -- display lines as one long line
-  scrolloff = 8,                           -- is one of my fav
-  sidescrolloff = 8,
   list = true,                             -- show whitespace chars
-  startofline = true,                    -- retain cursor position where possible
 }
 
 vim.opt.shortmess:append "c"
 
-for k, v in pairs(options) do
+for k, v in pairs(shared_options) do
   vim.opt[k] = v
+end
+
+if vim.fn.exists("g:vscode") ~= 0 then
+  -- VScode-nvim
+else
+  -- Standalone nvim
+  for k, v in pairs(standalone_options) do
+    vim.opt[k] = v
+  end
 end
 
