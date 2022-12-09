28638680e7674ce06611a36b91821eee3edc18ab
Gethin Davies <gethin.davies@motilent.co.uk>
2022-12-09 01:37:14 +0000

    Fix permissions

--- home/.chezmoiscripts/run_once_after_chsh.sh
+++ home/.chezmoiscripts/run_once_after_chsh.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 set -eufo pipefail
 
 echo "Changing shell..."
 sudo sh -c "echo \"$(brew --prefix)/bin/zsh\" >> /etc/shells"
-chsh -s "$(brew --prefix)/bin/zsh"
+sudo chsh -s "$(brew --prefix)/bin/zsh" $USER
