c9adf06aa38a4052cd8fd0dccf36b76d9289672f
Gethin Davies <gethin.davies@motilent.co.uk>
2022-12-09 03:00:14 +0000

    Add symlink to zsh install
    symlink to brew binary from a more conventional location

--- home/.chezmoiscripts/run_once_after_chsh.sh
+++ home/.chezmoiscripts/run_once_after_chsh.sh
@@ -3,5 +3,9 @@
 set -eufo pipefail
 
 echo "Changing shell..."
+# Add the homebrew shell to list of allowed shells
 sudo sh -c "echo \"$(brew --prefix)/bin/zsh\" >> /etc/shells"
+# Change shell
 sudo chsh -s "$(brew --prefix)/bin/zsh" $USER
+# Symlink to the installed version
+sudo ln -s "$(brew --prefix)/bin/zsh" /usr/bin/zsh
