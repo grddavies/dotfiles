9a6dcd7617b84540a5ff7e3a658456ea16449485
Gethin Davies <grd.davies@gmail.com>
2022-12-12 16:06:50 +0000

    Fallback if cant symlink in /usr/bin on macOS

--- home/.chezmoiscripts/run_once_after_chsh.sh
+++ home/.chezmoiscripts/run_once_after_chsh.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-set -eufo pipefail
+set -euo pipefail
 
 echo "Changing shell..."
 # Add the homebrew shell to list of allowed shells
 sudo sh -c "echo \"$(brew --prefix)/bin/zsh\" >> /etc/shells"
 # Change shell
 sudo chsh -s "$(brew --prefix)/bin/zsh" $USER
-# Symlink to the installed version
+# Try symlink to the installed version
 if [[ ! -f /usr/bin/zsh ]]; then
-sudo ln -s "$(brew --prefix)/bin/zsh" /usr/bin/zsh
+    sudo ln -s "$(brew --prefix)/bin/zsh" /usr/bin/zsh || echo "Could not link homebrew zsh to /usr/bin/zsh"
 fi
