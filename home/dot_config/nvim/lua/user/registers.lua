2e12bf6403282a6766f9b7a0e66a75db39fb87a6
Gethin Davies <grd.davies@gmail.com>
2022-12-25 03:17:48 +0100

    Add helper for clearing registers

---
+++ home/dot_config/nvim/lua/user/registers.lua
@@ -0,0 +1,10 @@
+
+local function clear_registers(args)
+  for reg in string.gmatch(args.args, ".") do
+    vim.fn.setreg(reg, {})
+  end
+end
+
+vim.api.nvim_create_user_command("ClearReg", clear_registers, {
+  nargs = 1
+})
