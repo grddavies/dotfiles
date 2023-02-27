873b68e2b089bf3146f62c1a71b0b3039c2620f3
Gethin Davies <gethin.davies@motilent.co.uk>
2023-02-27 11:17:45 +0000

    notion-snap -> notion-app
    Switch unofficial notion app bundles for improved performance

--- home/.chezmoiscripts/linux/run_onchange_before_install-apt-packages.sh
+++ home/.chezmoiscripts/linux/run_onchange_before_install-apt-packages.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 echo "Installing apt packages..."
+
+# Notion-Enhanced source
+echo "deb [trusted=yes] https://apt.fury.io/notion-repackaged/ /" | sudo tee /etc/apt/sources.list.d/notion-repackaged.list
+
 sudo apt-get update -qq && sudo apt-get upgrade -qq
 
 sudo apt-get install -qq \
@@ -12,3 +16,5 @@ sudo apt-get install -qq \
     jq \
     wget \
     xclip \
+    notion-app \
+
