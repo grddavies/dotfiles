fe561fd2ebc0211236f43afa079721ec1deb86d0
Gethin Davies <gethin.davies@motilent.co.uk>
2023-04-27 13:24:20 +0100

    Add autocmd for setting filetype jsonc

--- home/dot_config/nvim/lua/config/autocmds.lua
+++ home/dot_config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Set common json names to jsonc
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = {
+    ".eslintrc.json",
+    "tsconfig.json",
+    "keybindings.json", -- vscode keybindings
+  },
+  callback = function()
+    vim.api.nvim_buf_set_option(0, "filetype", "jsonc")
+  end,
+})
