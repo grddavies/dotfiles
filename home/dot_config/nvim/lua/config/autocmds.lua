ba1d95b85ec6bc5adc879d15b0c48fc3650861b9
Gethin Davies <gethin.davies@motilent.co.uk>
2023-05-04 16:50:30 +0100

    Set compiler & makeprg in typescript projects
    To get around the inability for tsserver to provide workspace
    diagnostics this commit adds a autocommand to upadte compiler and
    makeprg so that running :make will populate the quickfix list with any
    compiler errors in the project
    
    NOTE: :make runs synchronously and blocks ui while compiling
    
    TODO: Run make asynchronoulsy, watch project files

--- home/dot_config/nvim/lua/config/autocmds.lua
+++ home/dot_config/nvim/lua/config/autocmds.lua
@@ -2,8 +2,13 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+local function augroup(name)
+  return vim.api.nvim_create_augroup(name, { clear = true })
+end
+
 -- Set common json names to jsonc
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  group = augroup("jsonc"),
   pattern = {
     ".eslintrc.json",
     "tsconfig.json",
@@ -16,11 +21,24 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 
 -- Close some additional filetypes with <q>
 vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("easy_close"),
   pattern = {
     "fugitive",
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
     vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
+
+-- Use tsc for :make in typescript projects
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("tsc_quickfix"),
+  pattern = {
+    "typescript",
+    "typescriptreact",
+  },
+  callback = function()
+    vim.cmd([[compiler tsc | setlocal makeprg=npm\ exec\ tsc]])
   end,
 })
