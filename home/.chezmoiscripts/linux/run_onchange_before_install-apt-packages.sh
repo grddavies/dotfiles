6efd7acb5c3897196b0ceb56bd82b2c3dabdb37d
Gethin Davies <gethin.davies@motilent.co.uk>
2023-08-06 23:01:24 +0100

    Add Obsidian

--- home/.chezmoiscripts/linux/run_onchange_before_install-apt-packages.sh
+++ home/.chezmoiscripts/linux/run_onchange_before_install-apt-packages.sh
@@ -8,13 +8,11 @@ echo "deb [trusted=yes] https://apt.fury.io/notion-repackaged/ /" | sudo tee /et
 sudo apt-get update -qq && sudo apt-get upgrade -qq
 
 sudo apt-get install -qq \
-    curl \
-    exa \
-    llvm \
-    neofetch \
-    nmap \
-    jq \
-    wget \
-    xclip \
-    notion-app \
-
+	curl \
+	exa \
+	jq \
+	llvm \
+	neofetch \
+	nmap \
+	wget \
+	xclip
