bc481617183083f48e3008de1d3ee2792e38b644
Gethin Davies <grd.davies@gmail.com>
2024-04-30 08:31:43 +0100

    Remove zsh sudo plugin

--- home/dot_config/zsh/plugins.zsh
+++ home/dot_config/zsh/plugins.zsh
@@ -1,9 +1,6 @@
 # Syntax highlighting
 source "${XDG_DATA_HOME:-$HOME/.local/share}/zsh/f-sy-h/F-Sy-H.plugin.zsh"
 
-# Add `sudo` with 'Esc-Esc'
-source "${XDG_DATA_HOME:-$HOME/.local/share}/zsh/sudo/sudo.plugin.zsh"
-
 # Load adsf version manager
 source "$HOME/.asdf/asdf.sh"
 
