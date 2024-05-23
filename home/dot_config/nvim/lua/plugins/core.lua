60353cff8966638afe123f88b9666e739ad55e3d
Gethin Davies <grd.davies@gmail.com>
2024-05-23 13:48:39 +0100

    nvim 10 supertab

--- home/dot_config/nvim/lua/plugins/core.lua
+++ home/dot_config/nvim/lua/plugins/core.lua
@@ -159,20 +159,9 @@ return {
       }
     end,
   },
-  -- Disable default <Tab> and <S-Tab> behavior in LuaSnip
-  {
-    "L3MON4D3/LuaSnip",
-    vscode = true,
-    keys = function()
-      return {}
-    end,
-  },
+  -- Nvim 10.x Supertab completion
   {
     "hrsh7th/nvim-cmp",
-    vscode = true,
-    dependencies = {
-      "hrsh7th/cmp-emoji",
-    },
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       local has_words_before = function()
@@ -181,17 +170,17 @@ return {
         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
       end
 
-      local luasnip = require("luasnip")
       local cmp = require("cmp")
 
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
+            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
             cmp.select_next_item()
-            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-            -- they way you will only jump inside the snippet region
-          elseif luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
+          elseif vim.snippet.active({ direction = 1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(1)
+            end)
           elseif has_words_before() then
             cmp.complete()
           else
@@ -201,13 +190,14 @@ return {
         ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
+          elseif vim.snippet.active({ direction = -1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(-1)
+            end)
           else
             fallback()
           end
         end, { "i", "s" }),
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
       })
     end,
   },
