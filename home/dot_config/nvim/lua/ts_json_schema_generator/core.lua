f9d90dab98dc681423c689eeec3221927e83ad8c
Gethin Davies <grd.davies@gmail.com>
2025-01-23 12:54:15 +0000

    Remove telescope from ts_json_schema_generator

--- home/dot_config/nvim/lua/ts_json_schema_generator/core.lua
+++ home/dot_config/nvim/lua/ts_json_schema_generator/core.lua
@@ -17,6 +17,7 @@ function M.generate_schema(symbol, opts)
 
   local stdout = {}
 
+  -- Can this be handled by vim.schedule?
   Job:new({
     command = cmd,
     args = args,
@@ -52,76 +53,14 @@ function M.generate_schema(symbol, opts)
   }):start()
 end
 
-local get_filename_fn = function()
-  local bufnr_name_cache = {}
-  return function(bufnr)
-    bufnr = vim.F.if_nil(bufnr, 0)
-    local c = bufnr_name_cache[bufnr]
-    if c then
-      return c
-    end
-
-    local n = vim.api.nvim_buf_get_name(bufnr)
-    bufnr_name_cache[bufnr] = n
-    return n
-  end
-end
-
-local function entry_maker(opts)
-  local bufnr = opts.bufnr or vim.api.nvim_get_current_buf()
-
-  local display_items = {
-    { remaining = true },
-  }
-
-  if opts.show_line then
-    table.insert(display_items, 1, { width = 6 })
-  end
-
-  local displayer = require("telescope.pickers.entry_display").create({
-    separator = " ",
-    items = display_items,
-  })
-
-  local make_display = function(entry)
-    local display_columns = {
-      entry.text,
-    }
-    if opts.show_line then
-      table.insert(display_columns, 1, { entry.lnum .. ":" .. entry.col, "TelescopeResultsLineNr" })
-    end
-
-    return displayer(display_columns)
-  end
-
-  local get_filename = get_filename_fn()
-  return function(entry)
-    local start_row, start_col, end_row, _ = vim.treesitter.get_node_range(entry.node)
-    local node_text = vim.treesitter.get_node_text(entry.node, bufnr)
-    return require("telescope.make_entry").set_default_entry_mt({
-      value = entry.node,
-      kind = entry.kind,
-      ordinal = node_text .. " " .. (entry.kind or "unknown"),
-      display = make_display,
-
-      node_text = node_text,
-
-      filename = get_filename(bufnr),
-      -- need to add one since the previewer substacts one
-      lnum = start_row + 1,
-      col = start_col,
-      text = node_text,
-      start = start_row,
-      finish = end_row,
-    }, opts)
-  end
-end
-
+--- Find types to generate a schema for in the current buffer
+---@param opts any
+---@return {kind: string, node: TSNode}
 function M.find_types(opts)
   local ok, parser = pcall(vim.treesitter.get_parser, opts.bufnr or 0, "typescript")
   if not ok then
     log.error("find_types", { msg = "No parser available - Install with `TSInstall typescript`" })
-    return
+    return {}
   end
 
   local tree = parser:parse()[1]
@@ -163,39 +102,34 @@ function M.find_types(opts)
 end
 
 function M.picker(opts)
-  opts.show_line = vim.F.if_nil(opts.show_line, true)
-
-  local finders = require("telescope.finders")
-  local pickers = require("telescope.pickers")
-  local actions = require("telescope.actions")
-  local action_state = require("telescope.actions.state")
-  local conf = require("telescope.config").values
-
   local results = M.find_types(opts)
 
-  opts.show_line = true
-  pickers
-    .new(opts, {
-      prompt_title = "Select a symbol to generate a JSON schema for",
-      finder = finders.new_table({
-        results = results,
-        entry_maker = opts.entry_maker or entry_maker(opts),
-      }),
-      previewer = conf.grep_previewer(opts),
-      sorter = conf.prefilter_sorter({
-        tag = "kind",
-        sorter = conf.generic_sorter(opts),
-      }),
-      attach_mappings = function(prompt_bufnr)
-        actions.select_default:replace(function()
-          actions.close(prompt_bufnr)
-          local selection = action_state.get_selected_entry()
-          M.generate_schema(selection.text)
-        end)
-        return true
-      end,
-    })
-    :find()
+  if vim.tbl_isempty(results) then
+    log.info("picker", { msg = "No results found" })
+    return
+  end
+
+  local bufnr = vim.api.nvim_get_current_buf()
+
+  local node_names = vim
+    .iter(results)
+    :map(function(item)
+      return {
+        name = vim.treesitter.get_node_text(item.node, bufnr),
+      }
+    end)
+    :totable()
+
+  -- TODO: make a picker-agnostic previewer
+  vim.ui.select(node_names, {
+    kind = "Class",
+    prompts = "Select a type to generate a JSON schema for",
+    format_item = function(item)
+      return item.name
+    end,
+  }, function(choice)
+    M.generate_schema(choice)
+  end)
 end
 
 return M
