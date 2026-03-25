fec7dd1927336ac86433d38c9401466f52d18736
Gethin Davies <grd.davies@gmail.com>
2026-03-25 12:25:52 +0000

    remove brew claude-agent-acp install
    Was removed from brew due to license incompatibility

--- home/.chezmoiscripts/run_once_after_install-claude-code.sh
+++ home/.chezmoiscripts/run_once_after_install-claude-code.sh
@@ -5,3 +5,14 @@ if ! command -v claude >/dev/null 2>&1; then
   echo "Installing Claude Code..."
   curl -fsSL https://claude.ai/install.sh | bash
 fi
+
+if ! command -v claude-agent-acp >/dev/null 2>&1; then
+  echo "Installing Claude Agent ACP..."
+  if ! command -v node >/dev/null 2>&1; then
+    echo "[WARN] Node.js is required to install claude-agent-acp"
+    echo "once installed, run:"
+    echo "npm install -g @zed-industries/claude-agent-acp"
+  else
+    npm install -g @zed-industries/claude-agent-acp
+  fi
+fi
