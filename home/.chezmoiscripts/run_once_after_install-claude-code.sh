0a3b64a4d0a88385f03c42d64c1a261bd807d404
Gethin Davies <grd.davies@gmail.com>
2026-03-25 12:43:27 +0000

    install claude-agent-acp with mise

--- home/.chezmoiscripts/run_once_after_install-claude-code.sh
+++ home/.chezmoiscripts/run_once_after_install-claude-code.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
+safe_source() {
+  if [ -f "$1" ]; then
+    # shellcheck source=/dev/null
+    . "$1"
+  else
+    echo "File '$1' not found, skipping."
+  fi
+}
+
+safe_source "$HOME/.profile"
+
 if ! command -v claude >/dev/null 2>&1; then
   # Install claude code
   echo "Installing Claude Code..."
   curl -fsSL https://claude.ai/install.sh | bash
-fi
-
-if ! command -v claude-agent-acp >/dev/null 2>&1; then
-  echo "Installing Claude Agent ACP..."
-  if ! command -v node >/dev/null 2>&1; then
-    echo "[WARN] Node.js is required to install claude-agent-acp"
-    echo "once installed, run:"
-    echo "npm install -g @zed-industries/claude-agent-acp"
-  else
-    npm install -g @zed-industries/claude-agent-acp
-  fi
 fi
